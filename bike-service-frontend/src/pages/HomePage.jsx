b05cbf3f66bfe4bdbab360f284f47363c775025a
kavinbr <115334023+kavinbr@users.noreply.github.com>
2024-11-15 14:20:04 +0530

    Update HomePage.jsx

--- bike-service-frontend/src/pages/HomePage.jsx
+++ bike-service-frontend/src/pages/HomePage.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
   const [contactFormData, setContactFormData] = useState({
     message: ''
   });
-  const baseUrl = "https://bikeservice-smo9.onrender.com"
+  const baseUrl = "https://bikeproject.onrender.com"
 
   useEffect(() => {
     const token = localStorage.getItem('token');
